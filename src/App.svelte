659cb03d41224a064661b9a901b8ac6ca5ae4507
Fumiya Nagaoka <nagaoka@goq.co.jp>
2020-05-29 12:51:18 +0900

    sass導入。と不要なglobal.cssを削除。

--- src/App.svelte
+++ src/App.svelte
@@ -4,19 +4,19 @@
   export let name;
 </script>
 
-<style>
+<style lang="scss">
   main {
     text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
   }
 
   @media (min-width: 640px) {
