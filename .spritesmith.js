7691019e4dd97a87bb2c24335d0f3225fbc97acd
Fumiya Nagaoka <nagaoka@goq.co.jp>
2020-05-29 12:59:28 +0900

    spritesmith移植

---
+++ .spritesmith.js
@@ -0,0 +1,77 @@
+module.exports = [
+  {
+    src: 'assets/spr/001-050/*.png',
+    destImage: 'public/img/sprite1.png',
+    destCSS: 'public/css/sprite1.css'
+  },
+  {
+    src: 'assets/spr/051-100/*.png',
+    destImage: 'public/img/sprite2.png',
+    destCSS: 'public/css/sprite2.css'
+  },
+  {
+    src: 'assets/spr/101-150/*.png',
+    destImage: 'public/img/sprite3.png',
+    destCSS: 'public/css/sprite3.css'
+  },
+  {
+    src: 'assets/spr/151-200/*.png',
+    destImage: 'public/img/sprite4.png',
+    destCSS: 'public/css/sprite4.css'
+  },
+  {
+    src: 'assets/spr/201-250/*.png',
+    destImage: 'public/img/sprite5.png',
+    destCSS: 'public/css/sprite5.css'
+  },
+  {
+    src: 'assets/spr/251-300/*.png',
+    destImage: 'public/img/sprite6.png',
+    destCSS: 'public/css/sprite6.css'
+  },
+  {
+    src: 'assets/spr/301-350/*.png',
+    destImage: 'public/img/sprite7.png',
+    destCSS: 'public/css/sprite7.css'
+  },
+  {
+    src: 'assets/spr/351-400/*.png',
+    destImage: 'public/img/sprite8.png',
+    destCSS: 'public/css/sprite8.css'
+  },
+  {
+    src: 'assets/spr/401-450/*.png',
+    destImage: 'public/img/sprite9.png',
+    destCSS: 'public/css/sprite9.css'
+  },
+  {
+    src: 'assets/spr/451-500/*.png',
+    destImage: 'public/img/sprite10.png',
+    destCSS: 'public/css/sprite10.css'
+  },
+  {
+    src: 'assets/spr/501-550/*.png',
+    destImage: 'public/img/sprite11.png',
+    destCSS: 'public/css/sprite11.css'
+  },
+  {
+    src: 'assets/spr/551-600/*.png',
+    destImage: 'public/img/sprite12.png',
+    destCSS: 'public/css/sprite12.css'
+  },
+  {
+    src: 'assets/spr/601-650/*.png',
+    destImage: 'public/img/sprite13.png',
+    destCSS: 'public/css/sprite13.css'
+  },
+  {
+    src: 'assets/spr/651-700/*.png',
+    destImage: 'public/img/sprite14.png',
+    destCSS: 'public/css/sprite14.css'
+  },
+  {
+    src: 'assets/spr/701-750/*.png',
+    destImage: 'public/img/sprite15.png',
+    destCSS: 'public/css/sprite15.css'
+  }
+]
