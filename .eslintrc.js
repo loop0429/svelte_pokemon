f227f03ebd0af77f8fbe09075b19429cc783a35c
Fumiya Nagaoka <nagaoka@goq.co.jp>
2020-07-02 12:23:20 +0900

    lintの設定とlistの内容に合わせての修正

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  plugins: ['svelte3'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  extends: ['standard'],
+  rules: {
+    'no-multiple-empty-lines': [2, {'max': 2}]
+  }
+}
