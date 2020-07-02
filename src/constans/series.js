4923c72258d386d9a9668c12ef6a0d784f42ba39
Fumiya Nagaoka <nagaoka@goq.co.jp>
2020-07-02 12:59:25 +0900

    各種データ

---
+++ src/constans/series.js
@@ -0,0 +1,22 @@
+export const SERIES = [
+  {
+    area: 'カントー地方',
+    numbers: '1-151'
+  },
+  {
+    area: 'ジョウト地方',
+    numbers: '152-251'
+  },
+  {
+    area: 'ホウエン地方',
+    numbers: '252-386'
+  },
+  {
+    area: 'シンオウ地方',
+    numbers: '387-493'
+  },
+  {
+    area: 'イッシュ地方',
+    numbers: '494-650'
+  }
+]
---
+++ src/constans/types.js
@@ -0,0 +1,74 @@
+export const TYPES = [
+  {
+    en: 'Normal',
+    ja: 'ノーマル'
+  },
+  {
+    en: 'Fire',
+    ja: 'ほのお'
+  },
+  {
+    en: 'Water',
+    ja: 'みず'
+  },
+  {
+    en: 'Grass',
+    ja: 'くさ'
+  },
+  {
+    en: 'Electric',
+    ja: 'でんき'
+  },
+  {
+    en: 'Ice',
+    ja: 'こおり'
+  },
+  {
+    en: 'Fighting',
+    ja: 'かくとう'
+  },
+  {
+    en: 'Poison',
+    ja: 'どく'
+  },
+  {
+    en: 'Ground',
+    ja: 'じめん'
+  },
+  {
+    en: 'Flying',
+    ja: 'ひこう'
+  },
+  {
+    en: 'Psychic',
+    ja: 'エスパー'
+  },
+  {
+    en: 'Bug',
+    ja: 'むし'
+  },
+  {
+    en: 'Rock',
+    ja: 'いわ'
+  },
+  {
+    en: 'Ghost',
+    ja: 'ゴースト'
+  },
+  {
+    en: 'Dragon',
+    ja: 'ドラゴン'
+  },
+  {
+    en: 'Dark',
+    ja: 'あく'
+  },
+  {
+    en: 'Steel',
+    ja: 'はがね'
+  },
+  {
+    en: 'Fairy',
+    ja: 'フェアリー'
+  }
+]
